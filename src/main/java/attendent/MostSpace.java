671ad09b99ac78ba4988b679b5b45ed746a87488
varsha <varshashobha1996@gmail.com>
2019-10-07 15:54:13 +0530

    [varsha] add. Attendent able to park in most capacity and most space in parkinglots

--- src/main/java/attendent/MostSpace.java
+++ src/main/java/attendent/MostSpace.java
@@ -3,15 +3,13 @@ package attendent;
 import exception.DuplicateException;
 import exception.ParkingFullException;
 import parkinglot.ParkingLot;
-
-import java.util.Collections;
 import java.util.List;
 
 public class MostSpace implements Parking {
 
     @Override
     public void park(List<ParkingLot> parkingLots, Object object) throws DuplicateException, ParkingFullException {
-        Collections.sort(parkingLots,new MostSpaceComparator());
+        parkingLots.sort(new MostSpaceComparator());
         parkingLots.get(parkingLots.size()-1).park(object);
     }
 }
